1bf7dc46b4fb1d4f1fc8c921917373d2d9b4f6fd
Juan Estrada <juansebastianestrada@gmail.com>
2019-05-15 21:37:02 -0400

    Updates

---
+++ precache-manifest.12cd56d491547af7dbd63532ca614220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c299b3a764303d082f5a",
+    "url": "/rc-fractals/static/js/runtime~main.769bc25f.js"
+  },
+  {
+    "revision": "6f0a5336d97b405cb0a5",
+    "url": "/rc-fractals/static/js/main.de3b8fc8.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rc-fractals/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6f0a5336d97b405cb0a5",
+    "url": "/rc-fractals/static/css/main.048b4cee.chunk.css"
+  },
+  {
+    "revision": "b5ae59151a1ec32f94597e9a667bcd7d",
+    "url": "/rc-fractals/index.html"
+  }
+];
